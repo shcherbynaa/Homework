7925b6b8ab6312f9cd878ba35188c9deb6aa9899
shcherbynaa <47137576+shcherbynaa@users.noreply.github.com>
2019-02-23 01:14:51 +0200

    1 task

--- src/Test.java
+++ src/Test.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Test {
 
     private static int a = 3;
-    private static int b = 1;
+    private static int b = 3;
     private static int c = 1;
     private static int d = 2;
     
     public static void main(String[] args){
-       int  result = ( ((a== c) && (b == d)) ? 2:((((a>b) && (c>d))) || ((a<b) && (c<d))) ?1:0);
+       int  result = ( ((a == c) && (b == d)) ? 2:((((a>b) && (c>d))) || ((a<b) && (c<d))) ?1:0);
         System.out.println(result);
     }
 
