100376b4f7ae3352ad34b51a153ae67b715a9724
shcherbynaa <47137576+shcherbynaa@users.noreply.github.com>
2019-02-23 12:24:20 +0200

    3 task, power

---
+++ src/Power.java
@@ -0,0 +1,20 @@
+public class Power {
+
+    public static double square(int num){
+        return Math.pow(num,2);
+    }
+
+    public static double cube(int num){
+        return Math.pow(num,3);
+    }
+
+    public static double power(int num, int pow){
+        return Math.pow(num, pow);
+    }
+
+    public static void main(String[] args){
+        System.out.println(square(2));
+        System.out.println(cube(2));
+        System.out.println(power(2,5));
+    }
+}
