8f405ad23915d2b88292594bef54895f1c8c27f3
shcherbynaa <47137576+shcherbynaa@users.noreply.github.com>
2019-02-24 18:11:29 +0200

    add .gitignore file

--- src/Rhombus.java
+++ src/Rhombus.java
@@ -1,4 +1,50 @@
 public class Rhombus {
 
+    public static void rhombus(int num){
+
+        int line = num * 2 -1;
+        int star = 1;
+        int zeroline = (line - star)/2;
+
+        for (int i = 0; i < num; i++) {
+
+            for (int k = 0; k < zeroline; k++) {
+                System.out.print(" ");
+            }
+            for (int k = zeroline; k < (zeroline + star); k++) {
+                System.out.print("* ");
+            }
+
+            star += 1;
+            zeroline-=1;
+            System.out.println();
+        }
+
+        star -= 2;
+        zeroline = (line - star)/2+1;
+
+
+
+        for (int i = num; i > 0 ; i--) {
+        /*    for (int k = num; k < zeroline; k++) {
+                System.out.print(" ");
+            }
+            */
+            for (int k = zeroline + star+1; k<line; k++){
+                System.out.print(" ");
+            }
+            for (int k = zeroline; k < (zeroline + star); k++) {
+                System.out.print("* ");
+            }
+            star -= 1;
+            zeroline+=0;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        rhombus(5);
+    }
+
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,13 @@
-import java.util.Scanner;
-
 public class Test {
 
-    private static int a = 3;
-    private static int b = 4;
-    private static int c = 1;
-    private static int d = 2;
+    public static void resultOfBet(int realHostScore, int realGuestScore, int betHostScore, int betGuestScore){
+        int  result = ( ((realHostScore == betHostScore) && (realGuestScore ==  betGuestScore)) ? 2:((((realHostScore>realGuestScore) &&
+                (betHostScore>betGuestScore))) || ((realHostScore<realGuestScore) && (betHostScore<betGuestScore))) ?1:0);
+        System.out.println(result);
+    }
     
     public static void main(String[] args){
-       int  result = ( ((a == c) && (b == d)) ? 2:((((a>b) && (c>d))) || ((a<b) && (c<d))) ?1:0);
-        System.out.println(result);
+      resultOfBet(3,4,3,2);
     }
 
 }
